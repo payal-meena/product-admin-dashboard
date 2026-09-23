b002dcf9e224d7168b3f743ff4688f93e37e51e5
payal-meena <payalm.bca2023@ssism.org>
2026-09-23 14:19:46 +0530

    Add shared Axios instance with interceptors

---
+++ src/lib/axios.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: "https://dummyjson.com",
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+axiosInstance.interceptors.response.use((response) => response, 
+(error) => {
+    if(error.response) {
+        console.error("API Error:", error.response.status, error.response.data);
+    } else {
+        console.error("Network Error:", error.message);
+    }
+    return Promise.reject(error);
+}
+);
+
+export default axiosInstance;
